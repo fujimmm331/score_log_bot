1785a203585d2b7c9871cbe9886d263bca8d1f8c
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-03-23 18:28:17 +0900

    feat:得点一覧を表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # post '/callback' => 'scores#callback'
   post '/callback' => 'line_bot#callback'
+
+  resources :scores, only: [:index, :edit, :update, :destroy]
+  root to: "scores#index"
 end
