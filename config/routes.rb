e0a5efb4a12c7bc53de3575986494207083c5199
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-03-24 10:11:36 +0900

    fix:削除するケースがないと判断し、変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # post '/callback' => 'scores#callback'
   post '/callback' => 'line_bot#callback'
 
-  resources :scores, only: [:index, :edit, :update, :destroy]
+  resources :scores, only: [:index, :edit, :update]
   root to: "scores#index"
 end
