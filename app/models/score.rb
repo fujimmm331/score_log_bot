ba06f3f2481bcb2e0d89984d5fc88c131f6e1e7f
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2021-06-13 18:34:03 +0900

    リファクタリング

--- app/models/score.rb
+++ app/models/score.rb
@@ -13,6 +13,24 @@ class Score < ApplicationRecord
     validates :pk_germany_score
   end
 
+  def self.total_wins
+    return "\n" + '【勝ち数】' + "\n" + 'フランス：' + Score.where("franse_score + pk_franse_score > germany_score + pk_germany_score").count.to_s + '勝' + "\n" + 'ドイツ　：' + Score.where("franse_score + pk_franse_score < germany_score + pk_germany_score").count.to_s + '勝' + "\n"
+  end
+
+  def self.scoring_rate
+    return "\n" + '【得点率】' + "\n" + 'フランス：' + ' ' + Score.average(:franse_score).round(1).to_s + '点' + "\n" + 'ドイツ　：' + ' ' + Score.average(:germany_score).round(1).to_s + '点' + "\n"
+  end
+
+  def self.total_scores
+    total_flance_score = Score.sum(:franse_score)
+    total_germany_score = Score.sum(:germany_score)
+    return  "\n" + '【得点】' + "\n" + 'フランス：' + ' ' + total_flance_score.to_s + '点' + "\n" + 'ドイツ　：' + ' ' + total_germany_score.to_s + '点' + "\n" + '合計　　：' + ' ' + (total_flance_score + total_germany_score).to_s + '点' + "\n"
+  end
+
+  def self.total_matches
+    return '【総試合数】' + "\n" +  Score.count.to_s + '試合' + "\n"
+  end
+
   def self.saved_from_message(params)
     scores = params.split(" ").map!(&:to_i)
     score = Score.new(franse_score: scores[0], germany_score: scores[1], pk_franse_score: scores[2], pk_germany_score: scores[3])
@@ -20,30 +38,15 @@ class Score < ApplicationRecord
     if score.save 
       text = ''
       text << if (scores[0] + scores[2] > scores[1] + scores[3]) 
-                '🎉㊗️フランスの勝ち㊗️🎉' + "\n" + 'ドイツは出直してきな😇' + "\n"
-              else 
-                '🎉㊗️ドイツの勝ち㊗️🎉' + "\n" + 'フランスは出直してきな😇' + "\n"
+                '🎉㊗️フランスの勝ち㊗️🎉' + "\n" + 'ドイツは出直してきな🖕😇🖕' + "\n" + "\n"
+              else
+                '🎉㊗️ドイツの勝ち㊗️🎉' + "\n" + 'フランスは出直してきな🖕😇🖕' + "\n" + "\n"
               end
 
-      text << "\n"
-      text << '【総試合数】' + "\n"
-      text << Score.count.to_s + '試合' + "\n"
-
-      text << "\n"
-      text << '【勝ち数】' + "\n"
-      text << 'フランス：' + Score.where("franse_score + pk_franse_score > germany_score + pk_germany_score").count.to_s + '勝' + "\n"
-      text << 'ドイツ　：' + Score.where("franse_score + pk_franse_score < germany_score + pk_germany_score").count.to_s + '勝' + "\n"
-
-      text << "\n"
-      text << '【得点率】' + "\n"
-      text << 'フランス：' + ' ' + Score.average(:franse_score).round(1).to_s + '点' + "\n"
-      text << 'ドイツ　：' + ' ' + Score.average(:germany_score).round(1).to_s + '点' + "\n"
-
-      text << "\n"
-      text << '【得点】' + "\n"
-      text << 'フランス：' + ' ' + Score.sum(:franse_score).to_s + '点' + "\n"
-      text << 'ドイツ　：' + ' ' + Score.sum(:germany_score).to_s + '点' + "\n"
-      text << '合計　　：' + ' ' + (Score.sum(:germany_score) + Score.sum(:franse_score)).to_s + '点' + "\n"
+      text << Score.total_matches
+      text << Score.total_wins
+      text << Score.scoring_rate
+      text << Score.total_scores
 
     else
       '失敗。。スコアは半角数字、半角スペースで送ってね！'
@@ -52,8 +55,7 @@ class Score < ApplicationRecord
 
   def self.result
     text = ''
-    text << '【総試合数】' + "\n"
-    text << Score.count.to_s + '試合' + "\n"
+    text << Score.total_matches
 
     text << "\n"
     text << '【直近５試合の結果】' + "\n"
@@ -64,25 +66,8 @@ class Score < ApplicationRecord
       text << "\n"
     end
 
-    text << "\n"
-    text << '【勝ち数】' + "\n"
-    text << 'フランス：' + Score.where("franse_score + pk_franse_score > germany_score + pk_germany_score").count.to_s + '勝' + "\n"
-    text << 'ドイツ　：' + Score.where("franse_score + pk_franse_score < germany_score + pk_germany_score").count.to_s + '勝' + "\n"
-
-    text << "\n"
-    text << '【得点率】' + "\n"
-    text << 'フランス：' + ' ' + Score.average(:franse_score).round(1).to_s + '点' + "\n"
-    text << 'ドイツ　：' + ' ' + Score.average(:germany_score).round(1).to_s + '点' + "\n"
-
-    text << "\n"
-    text << '【得点】' + "\n"
-    text << 'フランス：' + ' ' + Score.sum(:franse_score).to_s + '点' + "\n"
-    text << 'ドイツ　：' + ' ' + Score.sum(:germany_score).to_s + '点' + "\n"
-    text << '合計　　：' + ' ' + (Score.sum(:germany_score) + Score.sum(:franse_score)).to_s + '点' + "\n"
-
-    return text
+    text << Score.total_wins
+    text << Score.scoring_rate
+    text << Score.total_scores
   end
 end
-
-
-# select count(*) AS "総試合数",sum(case when franse_score + pk_franse_score > germany_score + pk_germany_score then 1 else 0 end) AS "フランスの勝ち数",sum(case when franse_score + pk_franse_score < germany_score + pk_germany_score then 1 else 0 end) AS "ドイツの勝ち数"from scores;
